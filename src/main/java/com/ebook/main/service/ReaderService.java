0889e67d81ef55e4c9035c6509ba7ef7a679670b
Biswajit Pradhan <pbiswajit967@gmail.com>
2023-02-04 21:54:28 +0530

    bookByBookName added in readercontroller class

--- src/main/java/com/ebook/main/service/ReaderService.java
+++ src/main/java/com/ebook/main/service/ReaderService.java
@@ -1,8 +1,12 @@
 package com.ebook.main.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ebook.main.model.Book;
 import com.ebook.main.model.Reader;
 import com.ebook.main.repository.ReaderRepository;
 
@@ -12,7 +16,19 @@ public class ReaderService {
 	@Autowired
 	private ReaderRepository readerRepository;
 	
+	@Autowired
+	private BookService bookService;
+	
 	public void addReader(Reader reader) {
 		readerRepository.save(reader);
 	}
+
+	public List<Book> getBookByBookName(String bName) {
+		List<Book> book = bookService.getAllBook().stream().filter(b->b.getName()
+				.equalsIgnoreCase(bName)).collect(Collectors.toList());
+		return book;
+	}
+
+
+	
 }
