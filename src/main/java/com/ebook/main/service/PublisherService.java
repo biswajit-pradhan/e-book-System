b748a462bef8f180f15b2f73657d702d4ac49c64
vishwa <vishwashahanedmb@gmail.com>
2023-02-04 21:42:38 +0530

    publisher crud API

--- src/main/java/com/ebook/main/service/PublisherService.java
+++ src/main/java/com/ebook/main/service/PublisherService.java
@@ -1,5 +1,8 @@
 package com.ebook.main.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,25 @@ public class PublisherService {
 
 	public void addPublisher(Publisher publisher) {
 		publisherRepository.save(publisher);
+		
+	}
+
+	public Optional<Publisher> getPublisherById(int id) {
+		Optional<Publisher> optional = publisherRepository.findById(id);
+		return optional;
+	}
+
+	public List<Publisher> getAllPublisher() {
+		return publisherRepository.findAll();
+	}
+
+	public void updatePublisher(Publisher publisher) {
+		publisherRepository.save(publisher);
+		
+	}
+
+	public void deletePublisher(int id) {
+		publisherRepository.deleteById(id);
 		
 	}
 
