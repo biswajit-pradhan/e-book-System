aa600449c01e67f70e35506619af405ba99e943b
Biswajit Pradhan <pbiswajit967@gmail.com>
2023-02-12 12:24:57 +0530

    authorShareOnIndividualBookRent added in AuthorController

--- src/main/java/com/ebook/main/service/AuthorService.java
+++ src/main/java/com/ebook/main/service/AuthorService.java
@@ -1,6 +1,9 @@
 package com.ebook.main.service;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import com.ebook.main.model.Author;
 import com.ebook.main.model.Book;
+import com.ebook.main.model.ReaderBook;
 import com.ebook.main.repository.AuthorRepository;
 import com.ebook.main.repository.ReaderBookRepository;
 
@@ -61,7 +65,41 @@ public class AuthorService {
 		List<Book> bookData=readerBookRepository.findAll().stream()
 				.map(rb->rb.getBook()).filter(b->bookIdsByAuthorName.contains(b.getId()))
 				.collect(Collectors.toList());
+		
+		Set<Book> bookDataSet=new HashSet<>(bookData);
+		bookData=new ArrayList<>(bookDataSet);
 		return bookData;
+	}
+	
+	public double authorShareOnIndividualBookRent(int bid) {
+		
+		double totalSum=0;
+		
+		List<ReaderBook> readerBookData=readerBookRepository.findAll().stream()
+										.filter(rb->rb.getBook().getId()==bid)
+										.collect(Collectors.toList());
+		
+		
+		for(ReaderBook rb:readerBookData) {
+			double sum=0;
+			int days=rb.getBorrowingDays();
+			double price=rb.getBook().getPrice();
+			if(days<=0)
+				sum=0;
+			else if(days<=7)
+				sum+=((price*10)/100);
+			else if(days<=14)
+				sum+=((price*15)/100);
+			else if(days<=21)
+				sum+=((price*20)/100);
+			else if(days<=30)
+				sum+=((price*25)/100);
+			else
+				sum+=((price*50)/100);
+			totalSum+=sum;
+		}
+		
+		return totalSum/2;
 	}
 
 }
