46b35df20847babb6a8a18f4f70636c4e7fb6806
Biswajit Pradhan <pbiswajit967@gmail.com>
2023-02-18 13:35:50 +0530

    Trending books and latest books done in front page

--- e-book-react/src/reducers/index.js
+++ e-book-react/src/reducers/index.js
@@ -3,8 +3,10 @@ import book from "./Book";
 import authorReducer from "./Author";
 import allPublisher from "./Reader/allPublisher";
 import allAuthor from "./Reader/allAuthor";
+import alllatestBook from "./Reader/alllatestBook";
 import userSignUp from "./User/SignUp/Index";
 import login from "./User/LogIn";
 import search from "./Search";
-export default combineReducers({book,allPublisher,allAuthor,userSignUp,login,search})
+import readerBook from "./ReaderBook"
+export default combineReducers({book,allPublisher,allAuthor,userSignUp,login,search,readerBook,alllatestBook})
 
--- src/main/java/com/ebook/main/controller/AuthorController.java
+++ src/main/java/com/ebook/main/controller/AuthorController.java
@@ -2,6 +2,7 @@ package com.ebook.main.controller;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +15,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ebook.main.bto.Message;
 import com.ebook.main.model.Author;
 import com.ebook.main.model.Book;
-import com.ebook.main.model.Message;
 import com.ebook.main.service.AuthorService;
 
 @RestController
