f7ae6105b9581eafa1dc42164534b0e76388d66a
Pranil Kate <pranilkate2000@gmail.com>
2023-02-19 17:56:14 +0530

    Data added

--- e-book-react/src/components/User/LogIn/schema.js
+++
@@ -1,8 +0,0 @@
-// import * as Yup from "yup";
-
-// export const loginSchema=Yup.object({
-//     username: Yup.string()
-//         .required("Please enter email!!")
-//         .matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}","Enter a valid email!!"),
-//     password: Yup.string().required("Please enter password!!")
-// });
--- e-book-react/src/components/User/SignUp/index.js
+++ e-book-react/src/components/User/SignUp/index.js
@@ -3,6 +3,9 @@ import "./style.css";
 import { connect } from "react-redux";
 import { addUserSignUp } from "../../../action/User/SignUp";
 import axios from "axios";
+import {Login} from "../LogIn"
+import { Link } from "react-router-dom";
+
 class SignUp extends Component {
     constructor(props) {
         super(props);
@@ -58,6 +61,7 @@ class SignUp extends Component {
                                             onChange={this.changeHandler}/>
                                         <span style={{color:'red'}}>{this.state.errors['password']}</span>
                                     </div>
+                                    <span style={{color:'green'}}>{this.state.msg}</span> <br />
                                     <div className="form-group">
                                         <br/>
                                         <input className="btn btn-primary btn-lg" type="submit" value="SignUp" onClick={this.onSignUp} />
@@ -81,14 +85,16 @@ class SignUp extends Component {
     }
     onSignUp = ()=>{
         if(this.handleValidation()){
-           // console.log(this.state.userSignUp);
-           this.postUser(this.state.userSignUp);
-        }
-        else{
-            console.log('validation not passed..');     
-        }
+            this.postUser(this.state.userSignUp);
+         }
+         else{
+             console.log('validation not passed..');     
+         }
     }
     handleValidation(){
+        const emailPattern=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const namePattern=/^[a-zA-Z\s]{2,25}$/;
+        const passwordPattern=/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
         let userName = this.state.userSignUp.userName;
         let emailId = this.state.userSignUp.emailId; 
         let password = this.state.userSignUp.password; 
@@ -97,14 +103,23 @@ class SignUp extends Component {
         if(!userName){
             formValid = false;
             tempErrors['userName']='Name cannot be empty';
+        }else if(!namePattern.test(userName)){
+            formValid = false;
+            tempErrors['userName']='Name must be at least 2 and maximum 25 characters';
         }
         if(!emailId){ 
             formValid = false;
             tempErrors['emailId']='Email cannot be empty';
+        }else if(!emailPattern.test(emailId)){
+            formValid = false;
+            tempErrors['emailId']='email not valid';
         }
         if(!password){ 
             formValid = false;
             tempErrors['password']='Password cannot be empty';
+        }else if(!passwordPattern.test(password)){ 
+            formValid = false;
+            tempErrors['password']='Password must contain 8 characters,one upper character and one captital letter';
         }
     
         this.setState({
@@ -119,7 +134,7 @@ class SignUp extends Component {
             console.log('API success');
             console.log(data);
             this.setState({
-                msg: "data.msg"
+                msg: "User Added"
             })
             this.props.addUserSignUp(data);
           } catch (error) {
